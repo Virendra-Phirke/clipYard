85272a9abac56fdb567bcd7d4bc471f0ecf29d2a
virendra  phirke <114152472+Virendra-Phirke@users.noreply.github.com>
2026-08-11 18:54:03 +0530

    chore: add documentation for FileTransferCardProps

--- modules/file-transfer/components/FileTransferCard.tsx
+++ modules/file-transfer/components/FileTransferCard.tsx
@@ -4,6 +4,9 @@ import { useState, type CSSProperties } from 'react'
 import type { Transfer } from '@/lib/webrtc/types'
 import { TransferProgress } from './TransferProgress'
 
+/**
+ * Props for the FileTransferCard component.
+ */
 interface FileTransferCardProps {
   transfer: Transfer
   onCancel?: (transferId: string) => void
